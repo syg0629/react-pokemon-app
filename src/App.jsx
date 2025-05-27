b139d8f2a6f5a75a60fc0ec181fa6ffe6bc7ed0b
syg0629 <syg0629@hanmail.net>
2025-05-27 23:42:34 +0900

    feat: 포켓몬 데이터 가져오기 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=1008&offset=0";
+
+  useEffect(() => {
+    fetchPokeData();
+  }, []);
+
+  const fetchPokeData = async () => {
+    try {
+      const response = await axios.get(url);
+      setPokemons(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world</h1>
-    </div>
+    <article className="pt-6">
+      <header className="flex flex-col gap-2 w-full px-4 z-50">
+        Input form
+      </header>
+      <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0"></section>
+      <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl"></div>
+      {pokemons.length > 0 ? (
+        pokemons.map(({ url, name }, index) => <div>{name}</div>)
+      ) : (
+        <h2 className="font-medium text-lg text-slate-900 mb-1">
+          포켓몬이 없습니다.
+        </h2>
+      )}
+    </article>
   );
 }
 
