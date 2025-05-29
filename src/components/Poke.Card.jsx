7f507e0e1de37d636f8891cab17d8c66874f5a97
syg0629 <syg0629@hanmail.net>
2025-05-29 20:41:07 +0900

    feat: 포켓몬 카드 컴포넌트 수정 및 색상 설정 추가

--- src/components/Poke.Card.jsx
+++ src/components/Poke.Card.jsx
@@ -2,17 +2,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const PokeCard = ({ url, name }) => {
-  const [pokemon, setPokemon] = useState(true);
+  const [pokemon, setPokemon] = useState();
 
   useEffect(() => {
     fetchPokeDetailData();
-  }, [pokemon]);
+  }, []);
 
   async function fetchPokeDetailData() {
     try {
       const response = await axios.get(url);
       const data = formatPokemonData(response.data);
-      console.log(data);
       setPokemon(data);
     } catch (error) {
       console.error(error);
@@ -24,12 +23,45 @@ const PokeCard = ({ url, name }) => {
     const pokeData = {
       id,
       name,
-      types: types[0].type.name,
+      type: types[0].type.name,
     };
     return pokeData;
   }
 
-  return <div>PokeCard</div>;
+  const bg = `bg-${pokemon?.type}`;
+  const border = `border-${pokemon?.type}`;
+  const text = `text-${pokemon?.type}`;
+
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+
+  return (
+    <>
+      {pokemon && (
+        <a
+          href={`/pokemon/${name}`}
+          className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}
+        >
+          <div
+            className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}
+          >
+            #{pokemon.id.toString().padStart(3, "00")}
+          </div>
+          <div className={`w-full f-6 flex items-center justify-center`}>
+            <div
+              className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}
+            >
+              <img src={img} alt={name} className={`object-contain h-full`} />
+            </div>
+          </div>
+          <div
+            className={`${bg} text-center text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1 `}
+          >
+            {pokemon.name}
+          </div>
+        </a>
+      )}
+    </>
+  );
 };
 
 export default PokeCard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './index.html',
-    './src/style/main.css'
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/style/main.css",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ffcb05",
+        second: "#3d7dca",
+        third: "#003a70",
+        normal: "#A8A77A",
+        fire: "#EE8103",
+        water: "#6390F0",
+        electric: "#dfbc30",
+        grass: "#7AC74C",
+        ice: "#97d4d2",
+        fighting: "#b83e3a",
+        poison: "#A33EA1",
+        ground: "#E2BF65",
+        flying: "#A98FF3",
+        psychic: "#F95587",
+        bug: "#A6B91A",
+        rock: "#B6A136",
+        ghost: "#735797",
+        dragon: "#6F35FC",
+        dark: "#705746",
+        steel: "#B7B7CE",
+        fairy: "#D685AD",
+        none: "#BfBfBf",
+      },
+    },
   },
   plugins: [],
-}
-
+};
