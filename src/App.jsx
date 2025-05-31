968bbc1fcb463e2dcaf1fee4a0feba1d6a731f8b
syg0629 <syg0629@hanmail.net>
2025-05-31 23:57:18 +0900

    feat: 포켓몬 카드 더보기 기능 생성

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import PokeCard from "./components/Poke.Card";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
 
   useEffect(() => {
-    fetchPokeData();
+    fetchPokeData(true);
   }, []);
 
-  const fetchPokeData = async () => {
+  const fetchPokeData = async (isFirstFetch) => {
     try {
+      const offsetValue = isFirstFetch ? 0 : offset + limit;
+      const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offsetValue}`;
       const response = await axios.get(url);
-      setPokemons(response.data.results);
+      setPokemons([...pokemons, ...response.data.results]);
+      setOffset(offsetValue);
     } catch (error) {
       console.error(error);
     }
@@ -25,17 +28,27 @@ function App() {
       <header className="flex flex-col gap-2 w-full px-4 z-50">
         Input form
       </header>
-      <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0"></section>
-      <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl"></div>
-      {pokemons.length > 0 ? (
-        pokemons.map(({ url, name }, index) => (
-          <PokeCard key={url} url={url} name={name} />
-        ))
-      ) : (
-        <h2 className="font-medium text-lg text-slate-900 mb-1">
-          포켓몬이 없습니다.
-        </h2>
-      )}
+      <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
+        <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
+          {pokemons.length > 0 ? (
+            pokemons.map(({ url, name }, index) => (
+              <PokeCard key={url} url={url} name={name} />
+            ))
+          ) : (
+            <h2 className="font-medium text-lg text-slate-900 mb-1">
+              포켓몬이 없습니다.
+            </h2>
+          )}
+        </div>
+      </section>
+      <div className={`text-center`}>
+        <button
+          onClick={() => fetchPokeData(false)}
+          className={`bg-slate-800 px-6 py-2 my-4 text-base rounded-lg text-white`}
+        >
+          더 보기
+        </button>
+      </div>
     </article>
   );
 }
