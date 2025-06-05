2561939edb9b71e1de3777efea91ac979e253b3b
syg0629 <syg0629@hanmail.net>
2025-06-05 22:46:46 +0900

    feat: AutoComplete 컴포넌트 추가 및 검색 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import PokeCard from "./components/Poke.Card";
-import { useDebounce } from "./hooks/useDebounce";
+// import { useDebounce } from "./hooks/useDebounce";
+import { AutoComplete } from "./components/AutoComplete";
 
 function App() {
   // 모든 포켓몬 데이터를 가지고 있는 state
@@ -13,8 +14,7 @@ function App() {
   const limitNum = 20;
   const url = `https://pokeapi.co/api/v2/pokemon?limit=1008&offset=0`;
 
-  const [searchTerm, setSearchTerm] = useState("");
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  // const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   const filterDisplayedPokemonData = (
     allPokemonsData,
@@ -30,9 +30,9 @@ function App() {
     fetchPokeData();
   }, []);
 
-  useEffect(() => {
-    handleSearchInput(debouncedSearchTerm);
-  }, [debouncedSearchTerm]);
+  // useEffect(() => {
+  //   handleSearchInput(debouncedSearchTerm);
+  // }, [debouncedSearchTerm]);
 
   const fetchPokeData = async () => {
     try {
@@ -47,46 +47,33 @@ function App() {
     }
   };
 
-  const handleSearchInput = async (searchTerm) => {
-    if (searchTerm.length > 0) {
-      try {
-        const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
-        );
-        const pokemonData = {
-          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
-          name: searchTerm,
-        };
-        setAllPokemons([pokemonData]);
-      } catch (error) {
-        setAllPokemons([]);
-        console.error("포켓몬을 찾을 수 없습니다.", error);
-      }
-    } else {
-      fetchPokeData(true);
-    }
-  };
+  // const handleSearchInput = async (searchTerm) => {
+  //   if (searchTerm.length > 0) {
+  //     try {
+  //       const response = await axios.get(
+  //         `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
+  //       );
+  //       const pokemonData = {
+  //         url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
+  //         name: searchTerm,
+  //       };
+  //       setAllPokemons([pokemonData]);
+  //     } catch (error) {
+  //       setAllPokemons([]);
+  //       console.error("포켓몬을 찾을 수 없습니다.", error);
+  //     }
+  //   } else {
+  //     fetchPokeData(true);
+  //   }
+  // };
 
   return (
     <article className="pt-6">
       <header className="flex flex-col gap-2 w-full px-4 z-50">
-        <div className="relative z-50">
-          <form className="relative flex justify-center items-center w-[20.5rem] h-6 rouded-lg m-auto">
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center"
-              placeholder="포켓몬 이름을 입력하세요"
-            />
-            <button
-              type="submit"
-              className="text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700"
-            >
-              검색
-            </button>
-          </form>
-        </div>
+        <AutoComplete
+          allPokemons={allPokemons}
+          setDisplayedPokemons={setDisplayedPokemons}
+        />
       </header>
       <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
         <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
