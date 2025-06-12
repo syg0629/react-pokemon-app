e81ef6dbb65b6b765b210f5eda401a5c85383bdc
syg0629 <syg0629@hanmail.net>
2025-06-12 23:45:45 +0900

    feat: 포켓몬 상세 페이지에서 데이터 포맷팅 및 로딩 상태 관리 추가

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const DetailPage = () => {
+  const [pokemon, setPokemon] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
   const params = useParams();
   const pokemonId = params.id;
   const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
@@ -21,11 +24,55 @@ const DetailPage = () => {
         const { name, id, types, weight, height, stats, abilities } =
           pokemonData;
         const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id);
+
+        const DamageRelations = await Promise.all(
+          types.map(async (i) => {
+            const type = await axios.get(i.type.url);
+            return type.data.damage_relations;
+          })
+        );
+
+        const formattedPokemonData = {
+          id,
+          name,
+          weight: weight / 10,
+          height: height / 10,
+          previous: nextAndPreviousPokemon.previous,
+          next: nextAndPreviousPokemon.next,
+          abilities: formatPokemonAbilities(abilities),
+          stats: formatPokemonStats(stats),
+          DamageRelations,
+        };
+
+        setPokemon(formattedPokemonData);
+        setIsLoading(false);
       }
     } catch (error) {
       console.error(error);
     }
   }
+  const formatPokemonStats = ([
+    statHP,
+    statATK,
+    statDEP,
+    statSARK,
+    statSDEP,
+    statSPD,
+  ]) => [
+    { name: "Hit Points", baseStat: statHP.base_stat },
+    { name: "Attack", baseStat: statATK.base_stat },
+    { name: "Defense", baseStat: statDEP.base_stat },
+    { name: "Special Attack", baseStat: statSARK.base_stat },
+    { name: "Special Defense", baseStat: statSDEP.base_stat },
+    { name: "Speed", baseStat: statSPD.base_stat },
+  ];
+
+  const formatPokemonAbilities = (abilities) => {
+    abilities
+      .filter((_, index) => index <= 1)
+      .map((obj) => obj.ability.name.replaceAll(("-", "")));
+    return abilities;
+  };
 
   async function getNextAndPreviousPokemon(id) {
     const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
@@ -40,6 +87,8 @@ const DetailPage = () => {
       previous: previousResponse?.data.results?.[0]?.name,
     };
   }
+
+  if (isLoading) return <div>...loading</div>;
 
   return <div>DetailPage</div>;
 };
