b3e103fe0dd660a3da3577defa996f3d02da9d4b
syg0629 <syg0629@hanmail.net>
2025-06-25 14:19:03 +0900

    feat: DamageRelations 컴포넌트에 상태 관리 및 중복 값 처리 로직 추가

--- src/components/DamageRelations.jsx
+++ src/components/DamageRelations.jsx
@@ -1,16 +1,68 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import Type from "./Type";
 
 const DamageRelations = ({ damages }) => {
-  useEffect(() => {
-    const arrayDamage = damages.map((damage) => {
-      separateObjectBetweenToAndFrom(damage);
-    });
+  const [damagePokemon, setDamagePokemon] = useState();
 
-    // if (arrayDamage.length === 2) {
-    // } else {
-    //   postDamageValue(arrayDamage[0].from);
-    // }
-  }, []);
+  useEffect(() => {
+    const arrayDamage = damages.map((damage) =>
+      separateObjectBetweenToAndFrom(damage)
+    );
+
+    if (arrayDamage.length === 2) {
+      const obj = joinDamageRelations(arrayDamage);
+      setDamagePokemon(reduceDuplicateValues(postDamageValue(obj.from)));
+    } else {
+      setDamagePokemon(postDamageValue(arrayDamage[0].from));
+    }
+  }, [damages]);
+
+  const joinDamageRelations = (props) => {
+    return {
+      to: joinObjects(props, "to"),
+      from: joinObjects(props, "from"),
+    };
+  };
+
+  const joinObjects = (props, string) => {
+    const key = string;
+    const firstArrayValue = props[0][key];
+    const secondArrayValue = props[1][key];
+
+    const result = Object.entries(secondArrayValue).reduce(
+      (acc, [keyName, value]) => {
+        const result = firstArrayValue[keyName].concat(value);
+        return (acc = { [keyName]: result, ...acc });
+      },
+      {}
+    );
+
+    return result;
+  };
+
+  const reduceDuplicateValues = (props) => {
+    const duplicateValues = {
+      double_damage: "4x",
+      half_damage: "1/4x",
+      no_damage: "0x",
+    };
+
+    return Object.entries(props).reduce((acc, [keyName, value]) => {
+      const key = keyName;
+      const verifiedValue = filterForUniqueValues(value, duplicateValues[key]);
+      return (acc = { [key]: verifiedValue, ...acc });
+    }, {});
+  };
+
+  const filterForUniqueValues = (valueForFiltering, damageValue) => {
+    return valueForFiltering.reduce((acc, currentValue) => {
+      const { url, name } = currentValue;
+      const filterAcc = acc.filter((a) => a.name !== name);
+      return filterAcc.length === acc.length
+        ? (acc = [currentValue, ...acc])
+        : (acc = [{ damageValue: damageValue, name, url }, ...filterAcc]);
+    }, []);
+  };
 
   const postDamageValue = (props) => {
     const result = Object.entries(props).reduce((acc, [keyName, value]) => {
@@ -21,7 +73,6 @@ const DamageRelations = ({ damages }) => {
         half_damage: "1/2x",
         no_damage: "0x",
       };
-      // console.log(acc, [keyName, value])
 
       return (acc = {
         [keyName]: value.map((i) => ({
@@ -55,7 +106,42 @@ const DamageRelations = ({ damages }) => {
     return result;
   };
 
-  return <div>DamageRelations</div>;
+  return (
+    <div className="flex gap-2 flex-col">
+      {damagePokemon ? (
+        <>
+          {Object.entries(damagePokemon).map(([keyName, value]) => {
+            const key = keyName;
+            const valuesOfKeyName = {
+              double_damage: "Weak",
+              half_damage: "Registant",
+              no_damage: "Immune",
+            };
+            return (
+              <div key={key}>
+                <h3 className="capitalize font-medium text-sm md:text-base text-slate-500 text-center">
+                  {valuesOfKeyName[key]}
+                </h3>
+                <div className="flex flex-wrap gap-1 justify-center">
+                  {value.length > 0 ? (
+                    value.map(({ name, url, damageValue }) => {
+                      return (
+                        <Type type={name} key={url} damageValue={damageValue} />
+                      );
+                    })
+                  ) : (
+                    <Type type={"none"} key={"none"} />
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default DamageRelations;
