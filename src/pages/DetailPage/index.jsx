cdb95a41cac7048b8aae23eb93e36da293cae1f3
syg0629 <syg0629@hanmail.net>
2025-06-28 23:55:36 +0900

    feat: 포켓몬 설명을 가져오는 기능 추가 및 상세 페이지에 표시

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -55,6 +55,7 @@ const DetailPage = () => {
           DamageRelations,
           types: types.map((type) => type.type.name),
           sprites: formatPokemonSprites(sprites),
+          description: await getPokemonDescription(id),
         };
 
         setPokemon(formattedPokemonData);
@@ -64,6 +65,23 @@ const DetailPage = () => {
       console.error(error);
     }
   }
+
+  const getPokemonDescription = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${id}/`;
+    const { data: pokemonSpecies } = await axios.get(url);
+
+    const descriptions = filterAndFormatDescription(
+      pokemonSpecies.flavor_text_entries
+    );
+    return descriptions[Math.floor(Math.random() * descriptions.length)];
+  };
+
+  const filterAndFormatDescription = (flavorText) => {
+    const koreanDescriptions = flavorText
+      ?.filter((text) => text.language.name === "ko")
+      .map((text) => text.flavor_text.replace(/\r|\n|\f/g, " "));
+    return koreanDescriptions;
+  };
 
   const formatPokemonSprites = (sprites) => {
     const newSprites = { ...sprites };
@@ -229,6 +247,11 @@ const DetailPage = () => {
               </tbody>
             </table>
           </div>
+
+          <h2 className={`text-base font-semibold ${text}`}>설명</h2>
+          <p className="text-md leading-4 font-sans text-zinc-200 max-w-[30rem] text-center">
+            {pokemon.description}
+          </p>
 
           <div className="flex my-8 flex-wrap justify-center">
             {pokemon.sprites.map((url, index) => (
