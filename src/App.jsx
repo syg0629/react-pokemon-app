dc1d5b04c635c1e7e67f3ab09529dfec8f61f3cf
syg0629 <syg0629@hanmail.net>
2025-07-01 23:26:41 +0900

    feat: 레이아웃 구성 및 내비게이션 바 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DetailPage from "./pages/DetailPage";
+import LoginPage from "./pages/LoginPage";
+import NavBar from "./components/NavBar";
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <br />
+      <br />
+      <br />
+      <Outlet />
+    </>
+  );
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/pokemon/:id" element={<DetailPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/pokemon/:id" element={<DetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
