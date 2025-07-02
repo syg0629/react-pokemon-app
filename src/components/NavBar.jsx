698d826a8040cf995db8b84d8e197152c99a9a91
syg0629 <syg0629@hanmail.net>
2025-07-02 23:59:18 +0900

    feat: 내비게이션 바 컴포넌트 추가 및 스타일링(Styled Component사용)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <NavWrapper>
+      <Logo>
+        <Image
+          alt="Poke logo"
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+          onClick={() => (window.location.href = "/")}
+        />
+      </Logo>
+    </NavWrapper>
+  );
 };
+
+const Image = styled.img`
+  cursor: pointer;
+  width: 100%;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 50px;
+  margin-top: 4px;
+`;
+
+const NavWrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 100;
+`;
 
 export default NavBar;
