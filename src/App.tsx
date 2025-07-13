87cad498015e7aeac1b202ccfbde94e88d419603
syg0629 <syg0629@hanmail.net>
2025-07-13 17:03:07 +0900

    feat: uuid 타입 정의 추가 및 관련 패키지 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+// uuid는 js로 만들어진 파일이기 때문에 ts프로젝트에서 오류가 남.
+// uuid.d.ts 파일을 만들어서 타입을 정의해주면 오류가 사라짐.(자바스크립트로 된 모듈에 type을 지정해 주는 방법 => .d.ts파일 생성)
+// 그런데 uuid.d.ts가 아닌 다른 파일을 만들면 아래왁 같은 삼중 주석으로 path를 정해주면 사용 가능.
+//// <reference path="./main.d.ts" />
+// 하지만 https://github.com/DefinitelyTyped/DefinitelyTyped에 있는 이미 정의된 타입을 가져다 쓰는 것이 좋음.
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DetailPage from "./pages/DetailPage";
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar";
+import { v4 } from "uuid";
 
 const Layout = () => {
   return (
---
+++ src/main.d.ts
@@ -0,0 +1,4 @@
+// App.tsx에 있는 주석 참고
+declare module "uuid" {
+  export const v4: () => string;
+}
